f07d3428219b84739f24c0b1d63347bae1c8f0b1
4rMyMen4 <paksiap8@gmail.com>
2020-03-04 13:41:02 +0700

    jadiin satu di server.js

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var sql = require('mssql')
+var cors = require('cors');
 var app = express();
+var bodyParser = require('body-parser');
 const hostname = '10.199.14.46';
 const port = 8013;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors())
+
 var productcontroller = require('./controller/productcontroller.js')();
+
+var conn =
+{
+        user: 'su',
+        password: 'SaSa1212',
+        server: '10.199.13.253',
+        database: 'nrp05111740000076'
+};
 
 app.get("/",function(request, response)
 {
@@ -11,7 +26,48 @@ app.get("/",function(request, response)
 });
 app.use("/api/product", productcontroller);
 
-app.listen(port, function () {
+var query = function (res, query, params) {
+    sql.connect(conn, function (err) {
+       if (err) {
+          res.end('Connection Error\n' + err)
+       }
+       else {
+          var request = new sql.Request()
+          if (params != null){
+             params.forEach(function (p) {
+                request.input(p.name, p.sqltype, p.value);
+             });
+          }
+          request.query(query, function (err, recordset) {
+             if (err) {
+                console.log('Query Error\n' + err)
+             }
+             else {
+                res.send(recordset)
+             }
+          })
+       }
+    })
+}
+
+app.get("/api/Mahasiswa",function(req, res)
+        {
+                var sqlQuery = "Select * from Mahasiswa";
+                query(res, sqlQuery, null);
+        }
+);
+
+app.post('/api/Mahasiswa',function(req,res){
+    var param = [
+       { name: 'NRP', sqltype: sql.Char, value: req.body.NRP },
+       { name: 'Nama', sqltype: sql.VarChar, value: req.body.Nama }
+     ]
+     
+     var qr = "insert into mahasiswa (NRP,Nama) values (@NRP, @Nama);"
+     query(res, qr, param);
+ })
+
+ app.listen(port, function () {
     var message = "Server runnning on Port: " + port;
-    console.log(message);
+    console.log('CORS-enabled web server listening on port 8013');
 });
